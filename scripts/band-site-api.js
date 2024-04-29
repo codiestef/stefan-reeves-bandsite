7386d10ced84161b853ff429a074abc0364dfdf0
Stefan Reeves <stefanreeves24@gmail.com>
2024-04-28 19:40:57 -0700

    complete get post functionality with array sorted by timestamp

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,12 +1,4 @@
-// const apiKey = "2491824a-7c92-4e17-a0c9-5cef7929cf54";
 
-// async function getComments(){
-//     const response = await axios.get(`https://unit-2-project-api-25c1595833b2.herokuapp.com/comments?api_key=${apiKey}`);
-//     console.log(response);
-    // // use response.data, which is an array of data, to loop and append elements to the DOM
-// }
-
-// getComments();
 
 class CommentApi {
     constructor(apiKey){
@@ -15,13 +7,16 @@ class CommentApi {
     }
     async getComments(){
         const response = await axios.get(`${this.baseURL}/comments?api_key=${this.apiKey}`);
-        // console.log(response);
-        return response.data; // array of comments
+        response.data.sort(function(a, b){
+            return b.timestamp - a.timestamp;
+        });
+
+        return response.data;
     }
 
     async postComment(newComment){
         const response = await axios.post(`${this.baseURL}/comments?api_key=${this.apiKey}`, newComment);
-        console.log(response);
+        return response.data;
     }
 }
 
@@ -30,7 +25,6 @@ const commentApi = new CommentApi("2491824a-7c92-4e17-a0c9-5cef7929cf54");
 console.log(commentApi);
 
 const commentListElement = document.querySelector(".comment-list");
-
 const commentFormElement = document.querySelector(".comment-form");
 
 commentFormElement.addEventListener("submit", async function(event){
@@ -45,12 +39,19 @@ commentFormElement.addEventListener("submit", async function(event){
         comment: event.target.commentReview.value
     }
 
-    commentApi.postComment(newComment);
+    const response = await commentApi.postComment(newComment);
+    console.log(response);
+    getCommentsAndAppendToDom();
+
 });
+
+
 
 async function getCommentsAndAppendToDom(){
     const comments = await commentApi.getComments();
     console.log(comments);
+
+    commentListElement.innerText = '';
 
     comments.forEach(function(comment){
         console.log(comment);
